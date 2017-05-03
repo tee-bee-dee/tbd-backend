22b93cae1ae3970bf7adac0f6da775809ae50b72
John Gamboa <johnhadriansgamboa@gmail.com>
2017-05-02 21:12:04 -0700

    require path

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
